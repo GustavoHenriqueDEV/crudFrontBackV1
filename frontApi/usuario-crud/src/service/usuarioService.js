1c7d75d9d87e0f5b2b9d71f403e2ae421d734114
Gustavo Henrique Bochnia Velho <gubochniavelho@gmail.com>
2024-11-08 15:46:53 -0300

    fix-service

--- frontApi/usuario-crud/src/service/usuarioService.js
+++ frontApi/usuario-crud/src/service/usuarioService.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 const API_URL = 'http://localhost:8080/usuarios'; // Altere para a URL do seu backend
 
 // Função para buscar todos os usuários
-export const getUsuarios = () => axios.get(`${API_URL}/usuarios`);
+export const getUsuarios = () => axios.get(`${API_URL}`);
 
 // Função para buscar um usuário por ID
 export const getUsuarioById = (id) => axios.get(`${API_URL}/usuarios/${id}`);
 
 // Função para criar um novo usuário
-export const createUsuario = (usuario) => axios.post(`${API_URL}/usuarios`, usuario);
+export const createUsuario = (usuario) => axios.post(`${API_URL}`, usuario);
 
 // Função para atualizar um usuário
-export const updateUsuario = (id, usuario) => axios.put(`${API_URL}/usuarios/${id}`, usuario);
+export const updateUsuario = (id, usuario) => axios.put(`${API_URL}/${id}`, usuario);
 
 // Função para deletar um usuário
-export const deleteUsuario = (id) => axios.delete(`${API_URL}/usuarios/${id}`);
+export const deleteUsuario = (id) => axios.delete(`${API_URL}/${id}`);
