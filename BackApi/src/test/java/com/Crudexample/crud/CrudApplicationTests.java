98907a51b8a4040cf82550440f6a0ee160fb7dc3
Gustavo Henrique Bochnia Velho <gubochniavelho@gmail.com>
2024-11-13 15:20:22 -0300

    insert-user-create-test-Junit

--- BackApi/src/test/java/com/Crudexample/crud/CrudApplicationTests.java
+++ BackApi/src/test/java/com/Crudexample/crud/CrudApplicationTests.java
@@ -1,13 +1,43 @@
 package com.Crudexample.crud;
 
+import com.Crudexample.crud.model.Usuario;
+import com.Crudexample.crud.repository.UsuarioRepository;
+import com.Crudexample.crud.service.UsuarioService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class CrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Mock
+	private UsuarioRepository usuarioRepository;
 
+	@InjectMocks
+	private UsuarioService usuarioService;
+
+	@Test
+	public void testaCreateUser(){
+
+
+		Usuario mockUsuario = new Usuario(1L, "João Silva", "joao123", "senhaSegura", "joao@example.com", 30);
+		when(usuarioRepository.save(mockUsuario)).thenReturn(mockUsuario);
+
+
+		Usuario usuarioResult = usuarioService.create(mockUsuario);
+
+		assertNotNull(usuarioResult);
+		assertNotNull(usuarioResult.getIdusuario());
+		assertEquals("João Silva", usuarioResult.getNome());
+		assertEquals("joao123", usuarioResult.getLogin());
+		assertEquals("senhaSegura", usuarioResult.getSenha());
+		assertEquals(30, usuarioResult.getIdade());
+		assertEquals("joao@example.com", usuarioResult.getEmail());
+
+		verify(usuarioRepository, times(1)).save(usuarioResult);
+
+ 	}
 }
--- frontApi/usuario-crud/src/service/usuarioService.js
+++ frontApi/usuario-crud/src/service/usuarioService.js
@@ -1,18 +1,13 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/usuarios'; // Altere para a URL do seu backend
+const API_URL = 'http://localhost:8080/usuarios'; 
 
-// Função para buscar todos os usuários
 export const getUsuarios = () => axios.get(`${API_URL}`);
 
-// Função para buscar um usuário por ID
 export const getUsuarioById = (id) => axios.get(`${API_URL}/${id}`);
 
-// Função para criar um novo usuário
 export const createUsuario = (usuario) => axios.post(`${API_URL}`, usuario);
 
-// Função para atualizar um usuário
 export const updateUsuario = (id, usuario) => axios.put(`${API_URL}/${id}`, usuario);
 
-// Função para deletar um usuário
 export const deleteUsuario = (id) => axios.delete(`${API_URL}/${id}`);
